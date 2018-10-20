cd1eeba96e89f5dfa0860f85f06466403022231a
meil <ics.sora.meil@gmail.com>
2018-10-20 19:23:13 +0900

    プロジェクト ファイルを追加します。

---
+++ DefaultParameterSample.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DefaultParameterSample.Core;
+
+namespace DefaultParameterSample.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = new Sample();
+
+            System.Console.WriteLine($"--- {nameof(Sample.ConstString)} ---");
+            System.Console.WriteLine(Sample.ConstString);
+            System.Console.WriteLine($"--- {nameof(Sample.ReadonlyString)} ---");
+            System.Console.WriteLine(Sample.ReadonlyString);
+            System.Console.WriteLine($"--- {nameof(Sample.Method)} ---");
+            sample.Method();
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ DefaultParameterSample.Core/Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefaultParameterSample.Core
+{
+    public class Sample
+    {
+        public const string ConstString = "const_string: 1";
+        public static readonly string ReadonlyString = ConstString;
+
+        public void Method(string value = ConstString)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
